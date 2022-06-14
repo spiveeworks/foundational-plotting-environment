2167f244a2129f83c4b4bc7ffe39850188783d2d
SpiveeWorks <spiveehere@gmail.com>
2022-06-14 11:06:55 +1000

    space to pause, sleep for 1/60th of a second
    Also removed freetype stuff and fill_rect, and pulled the draw logic out
    into a function to emphasise it.

--- main.c
+++ main.c
@@ -2,203 +2,61 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <error.h>
-#include <errno.h>
+#include <string.h>
+#include <time.h>
 
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
-#include <X11/Xos.h>
 
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <X11/extensions/XShm.h>
 
-#include <ft2build.h>
-#include FT_FREETYPE_H
+void draw_frame(XImage *back_buf, long t) {
+    int width = back_buf->width;
+    int height = back_buf->height;
 
-Display *dis;
-Window win;
-GC gc;
+    long y[width];
+    char *data = back_buf->data + back_buf->xoffset;
+    int stride = back_buf->bytes_per_line;
 
-FT_Library  library;
-FT_Face     face;
+    long z = t % 512;
+    if (z > 256) z = 512 - z;
 
-typedef struct {
-    unsigned char b;
-    unsigned char g;
-    unsigned char r;
-    unsigned char a;
-} RGBA;
+    for (int j = 0; j < height; j++) {
+        long y = j - height / 2;
+        unsigned *row = (unsigned*)&data[stride * j];
+        for (int i = 0; i < width; i++) {
+            long x = i - width / 2;
 
-typedef struct str {
-    unsigned char *data;
-    size_t size;
-} str;
+            long r = z + x;
+            long g = z - y * 221 / 256 - x / 2;
+            long b = z + y * 221 / 256 - x / 2;
 
-#define GLYPH_COUNT 256
-struct Glyph {
-    int width;
-    int height;
-    int bearingx;
-    int bearingy; /* ascent in XLib terms. */
-    int advance;
-    /* Glyphs have a single value/opacity channel, which we then blend onto the
-     * screen ourselves. */
-    unsigned char *data;
-};
-
-struct Glyph glyphs[GLYPH_COUNT];
-typedef struct Glyph *Glyph;
-
-void init_text(void) {
-    FT_Error error = FT_Init_FreeType(&library);
-    if (error) {
-        fprintf(stderr, "ERROR: failed to initialise FreeType library\n");
-        exit(1);
-    }
-
-    error = FT_New_Face(
-        library,
-        "data/DejaVuSans.ttf",
-         0,
-         &face
-    );
-    if (error == FT_Err_Unknown_File_Format) {
-        fprintf(stderr, "ERROR: unknown font format\n");
-        exit(1);
-    } else if (error) {
-        fprintf(stderr, "ERROR: failed to load font\n");
-        exit(1);
-    }
-
-    error = FT_Set_Pixel_Sizes(face, 0, 16);
-    if (error) {
-        fprintf(stderr, "ERROR: failed to set char size\n");
-        exit(1);
-    }
-
-    for (int i = 0; i < GLYPH_COUNT; i++) {
-        error = FT_Load_Char(face, i, 0);
-        if (error)
-            continue;  /* ignore errors */
-
-        FT_Glyph_Metrics *metrics = &face->glyph->metrics;
-        int width = metrics->width >> 6;
-        int height = metrics->height >> 6;
-        glyphs[i].width = width;
-        glyphs[i].height = height;
-        glyphs[i].width = metrics->width >> 6;
-        glyphs[i].height = metrics->height >> 6;
-        glyphs[i].bearingx = metrics->horiBearingX >> 6;
-        glyphs[i].bearingy = metrics->horiBearingY >> 6;
-        glyphs[i].advance = metrics->horiAdvance >> 6;
-
-        glyphs[i].data = malloc(width * height);
-
-        /* load glyph image into the slot (erase previous one) */
-        error = FT_Load_Char(face, i, FT_LOAD_RENDER);
-        if (error)
-            continue;  /* ignore errors */
-
-        /* save a copy of it */
-        memcpy(glyphs[i].data, face->glyph->bitmap.buffer, width * height);
-    }
-}
-
-void string_dimensions(
-    str text,
-    int *ascent_out,
-    int *descent_out,
-    int *width_out
-) {
-    int ascent = 0;
-    int descent = 0;
-    int width = 0;
-    for (int i = 0; i < text.size; i++) {
-        unsigned char c = text.data[i];
-        int this_ascent = glyphs[c].bearingy;
-        int this_descent = glyphs[c].height - this_ascent;
-        if (this_ascent > ascent) ascent = this_ascent;
-        if (this_descent > descent) descent = this_descent;
-        width += glyphs[c].advance;
-    }
-    if (ascent_out) *ascent_out = ascent;
-    if (descent_out) *descent_out = descent;
-    if (width_out) *width_out = width;
-}
-
-void draw_string(XImage *out, int x, int y, str text) {
-    for (int ind = 0; ind < text.size; ind++) {
-        unsigned char c = text.data[ind];
-        int width = glyphs[c].width;
-        int height = glyphs[c].height;
-
-        char *out_data = out->data;
-        int bytes_per_line = out->bytes_per_line;
-
-        int corner_x = x + glyphs[c].bearingx;
-        int corner_y = y - glyphs[c].bearingy;
-        int start_i = corner_x < 0 ? -corner_x : 0;
-        int start_j = corner_y < 0 ? -corner_y : 0;
-
-        int end_i = corner_x + width > out->width ? out->width - corner_x : width;
-        int end_j = corner_y + height > out->height ? out->height - corner_y : height;
-
-        x += glyphs[c].advance;
-
-        if (end_i <= start_i) continue;
-        for (int j = start_j; j < end_j; j++) {
-            unsigned char *source_row = &glyphs[c].data[j * width];
-            RGBA *target_row = (RGBA*)&out_data[bytes_per_line * (corner_y + j)];
-            target_row += corner_x;
-            for (int i = start_i; i < end_i; i++) {
-                unsigned short a = source_row[i];
-                a = a + 50 - (a + 50) % 51; /* round up to multiple of 255/5 */
-                unsigned short b = 255 - a;
-                RGBA pix = target_row[i];
-                pix.r = a + b * pix.r / 256;
-                pix.g = a + b * pix.g / 256;
-                pix.b = a + b * pix.b / 256;
-                target_row[i] = pix;
+            if (r < 0 || r >= 256 || g < 0 || g >= 256 || b < 0 || b > 256) {
+                row[i] = 0;
+            } else {
+                row[i] = r << 16 | g << 8 | b;
             }
         }
     }
 }
 
-void fill_rectangle(XImage *out, int left, int top, int right, int bottom, RGBA colour_channels) {
-    if (left < 0) left = 0;
-    if (right >= out->width) right = out->width - 1;
-    if (top < 0) top = 0;
-    if (bottom >= out->height) bottom = out->height - 1;
+int main(int argc, char **argv) {
+    bool benchmark = false;
 
-    if (left > right) return;
-
-    union {
-        RGBA channels;
-        unsigned word;
-    } colour;
-    colour.channels = colour_channels;
-    unsigned word = colour.word;
-
-    for (int j = top; j <= bottom; j++) {
-        unsigned *target_row = (unsigned*)&out->data[out->bytes_per_line * j];
-        for (int i = left; i <= right; i++) {
-            target_row[i] = word;
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-benchmark") == 0) {
+            if (benchmark) fprintf(stderr, "Warning: \"-benchmark\" option appeared more than once.\n");
+            benchmark = true;
+        } else {
+            fprintf(stderr, "Warning: \"%s\" is not a known command line argument.\n", argv[i]);
         }
     }
-}
 
-
-int main(int argc, char **argv) {
-    bool key_down[256] = {};
-
-    {
-        dis = XOpenDisplay(NULL);
-        int screen = DefaultScreen(dis);
-
-        win = XCreateSimpleWindow(dis, DefaultRootWindow(dis), 0, 0,
+    Display *dis = XOpenDisplay(NULL);
+    Window win = XCreateSimpleWindow(dis, DefaultRootWindow(dis), 0, 0,
             600, 600, 0, 0xFFFFFF, 0x000000);
-    }
 
     if (!XShmQueryExtension(dis)) {
         fprintf(stderr, "Error: The shared memory extension is required for this program to run.\n");
@@ -215,7 +73,7 @@ int main(int argc, char **argv) {
         | StructureNotifyMask;
     XSelectInput(dis, win, mask);
 
-    gc = XCreateGC(dis, win, 0, 0);
+    GC gc = XCreateGC(dis, win, 0, 0);
 
     XClearWindow(dis, win);
     XMapRaised(dis, win);
@@ -228,14 +86,14 @@ int main(int argc, char **argv) {
     XImage *unscaled_buf = NULL;
     XShmSegmentInfo shm_info;
 
-    /* We could ask X for the initial mouse position. */
-    int mouse_x = 0;
-    int mouse_y = 0;
-
     bool waiting_for_shm_completion = false;
     const int SHM_COMPLETION = XShmGetEventBase (dis) + ShmCompletion;
 
     long t = 0;
+    bool play = true;
+
+    struct timespec frame_end;
+    clock_gettime(CLOCK_MONOTONIC_RAW, &frame_end);
 
     while (true) {
         XEvent event;
@@ -251,23 +109,8 @@ int main(int argc, char **argv) {
 
             if (XLookupString(&event.xkey, text, 255, &key, 0) == 1) {
                 if (key == XK_Escape) exit(EXIT_SUCCESS);
+                if (text[0] == ' ') play = !play;
             }
-        } else if (event.type == KeyRelease) {
-            /* Do nothing. */
-        } else if (event.type == ButtonPress) {
-            int button = event.xbutton.button;
-            if (button == 2) {
-                /* panning = true; */
-            }
-        } else if (event.type == ButtonRelease) {
-            if (event.xbutton.button == 2) {
-                /* panning = false; */
-            }
-        } else if (event.type == MotionNotify) {
-            mouse_x = event.xmotion.x;
-            mouse_y = event.xmotion.y;
-        } else if (event.type == FocusOut) {
-            for (int i = 0; i < 256; i++) key_down[i] = false;
         } else if (event.type == DestroyNotify) {
             exit(EXIT_SUCCESS);
         }
@@ -332,45 +175,32 @@ int main(int argc, char **argv) {
             back_buf_resize = false;
         }
 
-        int width = back_buf->width;
-        int height = back_buf->height;
-
-        long y[width];
-        char *data = back_buf->data + back_buf->xoffset;
-        int stride = back_buf->bytes_per_line;
-
-        long z = t % 1024;
-        for (int key_point = 64; key_point < 1024; key_point += 64) {
-            if (z > key_point) {
-                z -= 64;
-                if (z < key_point) z = key_point;
-            }
-        }
-        if (z > 256) z = 512 - z;
-
-        for (int j = 0; j < height; j++) {
-            long y = j - height / 2;
-            unsigned *row = (unsigned*)&data[stride * j];
-            for (int i = 0; i < width; i++) {
-                long x = i - width / 2;
-                long r = z + x;
-                long g = z - y * 221 / 256 - x / 2;
-                long b = z + y * 221 / 256 - x / 2;
-                if (r < 0 || r >= 256 || g < 0 || g >= 256 || b < 0 || b > 256) {
-                    row[i] = 0;
-                } else {
-                    row[i] = r << 16 | g << 8 | b;
-                }
-            }
-        }
+        draw_frame(back_buf, t);
 
         XShmPutImage (dis, win, gc, back_buf, 0, 0, 0, 0, window_width, window_height, true);
         waiting_for_shm_completion = true;
 
         XFlush(dis);
 
-        t += 1;
+        if (play) t += 1;
+
+        long second = 1000000000;
+        long frame = second / 60;
+
+        struct timespec now;
+        clock_gettime(CLOCK_MONOTONIC_RAW, &now);
+
+        long elapsed = (now.tv_sec - frame_end.tv_sec) * second
+            + (now.tv_nsec - frame_end.tv_nsec);
+
+        if (benchmark) printf("Took %dms.\n", elapsed / 1000000);
+
+        if (elapsed < frame) {
+            struct timespec remaining = {0, frame - elapsed};
+            nanosleep(&remaining, NULL);
+        }
+
+        clock_gettime(CLOCK_MONOTONIC_RAW, &frame_end);
     }
 }
-
 
